ec7f3abdbd090066fcb81db467b5f25c045e69f1
kbeazer <kbeazer@fullsail.edu>
2013-10-04 18:08:39 -0400

    WK1
    fso template added to JS practice file

--- WK1/goal2_assign_jspractice/js/main.js
+++ WK1/goal2_assign_jspractice/js/main.js
@@ -2,43 +2,118 @@
 // 10/4/13
 // JS Practice
 
+// self-executing function
+(function(){
 
-//create a function named 'avgNumbers'
-//- accept 1 parameter into the function that will be an array of unlimited numbers
-//- find the average of all the numbers
-//- return the average from the function
-//- console.log the answer outside of the function
+    /*******************************************
 
-var noLimit= [40,40];
-function avgNumbers(numbersArray){
-    var result= 0;
-    for(var i=0;i<numbersArray.length;i++){
-        result+= numbersArray[i];
-    } return result/(numbersArray.length)
-}console.log(avgNumbers(noLimit));
+     STUDENTS ASSIGNMENT
 
-//create a function named 'fullName'
-//- accept 2 parameters into the function that are strings (firstname and lastname)
-//- return the name after it has been concatenated
-//- console.log the answer outside of the function
+     1.  create a function named 'avgNumbers'
+     - accept 1 parameter into the function that will be an array of unlimited numbers
+     - find the average of all the numbers
+     - return the average from the function
+     - console.log the answer outside of the function
 
-var firstName= 'Kareem';
-var lastName= 'Beazer';
-function fullName(firstName, lastName){
-    return firstName+' '+lastName;
-}console.log(fullName(firstName, lastName))
+     2.  create a function named 'fullName'
+     - accept 2 parameters into the function that are strings (firstname and lastname)
+     - return the name after it has been concatenated
+     - console.log the answer outside of the function
 
-//create a function named 'wordCount'
-//- accept 1 parameter into the function that is a long string of text words
-//- create a function that counts all the words and return the answer
-//- console.log the answer outside of the function
+     3.  create a function named 'wordCount'
+     - accept 1 parameter into the function that is a long string of text words
+     - create a function that counts all the words and return the answer
+     - console.log the answer outside of the function
 
-var text= 'This is going to be a tough one to pull off';
-function wordCount(counter){
-   var words= 0;
-   for(var i=0;i<counter.length();i++){
-       if(counter==''){
-           words+=i;
-       }
-   }  return words+1;
-} console.log(wordCount(text));
+     4.  create a function named 'charCount'
+     - accept 1 parameter into the function that is a long string of text
+     - return length of the array of string characters
+     - console.log the answer outside of the function
+
+     5.  create a function named 'vowelsInWord'
+     - accept 1 parameter into the function that is a a one word string
+     - return the number of vowels in the word
+     - console.log the answer outside of the function
+
+     6.  create a function named 'findNum'
+     - accepts 2 parameters into the function - 1. array of numbers, 2. boolean
+     - if the second parameter being passed is "false" or null then
+     - create an array with all of the odd numbers from
+     the array
+     - else - create an array with all of the even numbers
+     from the array
+     - return the array
+     - console.log the answer outside of the function
+     ********************************************/
+
+    console.log('------ Goal2: Assignment: JavaScript Practice ----------');
+
+    console.log("1. avg of an array of numbers");
+
+    function avgNumbers(arr){
+        var result= 0;
+        for(var i=0;i<arr.length;i++){
+            result+= arr[i];
+    }       return result/arr.length;
+    }
+    console.log('avg number = ', avgNumbers([1,2,3,4,5]));
+
+    //--------------------------------------------------------
+    console.log("2. concat first and last name");
+
+    function fullName(firstName, lastName){
+        return firstName+lastName;
+    };
+
+    console.log(fullName('James', 'Bond'));
+
+    //--------------------------------------------------------
+    console.log("3. word count");
+    var ipsum = "this is test text that is being used as input to a function"
+    function wordCount(words){
+        var counter= 0;
+        for(var i=0;i<words.length;i++){
+            if(words==''){
+                counter+=i;
+            }
+        }  return words+1;
+    }
+    console.log(wordCount(ipsum));
+
+    //--------------------------------------------------------
+    console.log("4. sentence char count");
+
+    function charCount(letters){
+        var result= 0;
+        for(var i=0;i<letters.length;i++){
+            result+=letters[i];
+        } return result;
+    }
+
+    console.log(charCount(ipsum));
+
+    //--------------------------------------------------------
+    console.log("5. how many vowels in a word");
+
+    function vowelsInWord(vowelString){
+        var vowel= [a,e,i,o,u];
+        var vowAmount= 0;
+        for(var i=0;i<vowelString.length;i++){
+
+        }
+    }
+
+    console.log(vowelsInWord('JavaScript'));
+
+    //--------------------------------------------------------
+    console.log("6. find number and create an array of even or odd numbers");
+
+    function findNum([],odd){
+
+    }
+
+    console.log(findNum([31,22,4,67,83,6,5,4]));
+    console.log(findNum([31,22,4,67,83,6,5,4], false));
+
+})();
+
