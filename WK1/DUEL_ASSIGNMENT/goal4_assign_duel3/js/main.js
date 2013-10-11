464332725048d6b6db3e55061a0e0b82cba917fd
kbeazer <kbeazer@fullsail.edu>
2013-10-11 01:07:28 -0400

    Duel 3
    Duel 3 js file completed

--- WK1/DUEL_ASSIGNMENT/goal4_assign_duel3/js/main.js
+++ WK1/DUEL_ASSIGNMENT/goal4_assign_duel3/js/main.js
@@ -2,19 +2,14 @@
 // 10/1/13
 // The Dual Assignment
 
-var fighterArray= [{name: 'KABAL',
-                    damage: 50,
-                    health: 100},
-                   {name: 'KRATOS',
-                    damage: 50,
-                    health: 100}];
-var round= 0;
-var kabalDiv= document.getElementById("kabal"),
+var fighterArray= [{name: 'KABAL', damage: 50, health: 100}, {name: 'KRATOS', damage: 50, health: 100}];
+var round= 0,
+    kabalDiv= document.getElementById("kabal"),
     kratosDiv= document.getElementById("kratos"),
     roundDiv= document.getElementById("round"),
     fightButton= document.getElementById("fight_box");
-kabalDiv.innerHTML= fighterArray[0].name+' :  '+ fighterArray[0].health;
-kratosDiv.innerHTML= fighterArray[1].name+' :  '+ fighterArray[1].health;
+    kabalDiv.innerHTML= fighterArray[0].name+' :  '+ fighterArray[0].health;
+    kratosDiv.innerHTML= fighterArray[1].name+' :  '+ fighterArray[1].health;
 function kingFighter(){
     console.log('BEGIN!!!');
     fightButton.onclick= function fight(){
@@ -27,8 +22,8 @@ function kingFighter(){
         var damageResult= winnerSelector();
         if(damageResult== 'no winner'){
             round++;
-            kabalDiv.innerHTML= fighterArray[0][2];
-            kratosDiv.innerHTML= fighterArray[1][2];
+            kabalDiv.innerHTML= fighterArray[0].name+' :  '+ fighterArray[0].health;
+            kratosDiv.innerHTML= fighterArray[1].name+' :  '+ fighterArray[1].health;
             roundDiv.innerHTML= 'ROUND '+round+' OVER';
         }
         function winnerSelector(){
@@ -37,70 +32,34 @@ function kingFighter(){
                 kabalDiv.innerHTML='DOUBLE';
                 kratosDiv.innerHTML= 'KO!!!';
                 result= 'Double KO';
+                fightButton.onclick= document.getElementById("fight_box").disabled;
             }else if(fighterArray[0].health<1){
                 kabalDiv.innerHTML='GAME OVER';
                 kratosDiv.innerHTML= fighterArray[1].name+' WINS!!!';
                 result= 'Kratos Wins';
+                fightButton.onclick= document.getElementById("fight_box").disabled;
             }else if(fighterArray[1].health<1){
                 kabalDiv.innerHTML='GAME OVER';
                 kratosDiv.innerHTML= fighterArray[0].name+' WINS!!!';
                 result= 'Kabal Wins';
+                fightButton.onclick= document.getElementById("fight_box").disabled;
             }else if(round== 10 && fighterArray[0].health<fighterArray[1].health){
                 kabalDiv.innerHTML='GAME OVER';
                 kratosDiv.innerHTML= fighterArray[1].name+' WINS!!!';
                 result= 'Kratos Wins';
+                fightButton.onclick= document.getElementById("fight_box").disabled;
             }else if(round== 10 && fighterArray[0].health>fighterArray[1].health){
                 kabalDiv.innerHTML='GAME OVER';
                 kratosDiv.innerHTML= fighterArray[0].name+' WINS!!!';
                 result= 'Kabal Wins';
+                fightButton.onclick= document.getElementById("fight_box").disabled;
             }else if(round== 10 && fighterArray[0].health== fighterArray[1].health){
                 kabalDiv.innerHTML='GAME OVER';
                 kratosDiv.innerHTML= "IT'S A DRAW!!!";
                 result= "It's a Draw";
+                document.getElementById("fight_box").disabled= true;
             }return result;
     }
     }
 }kingFighter();
-//
-//    alert(leftFighter[0]+': '+leftFighter[1]+'    VS.    '+rightFighter[0]+': '+rightFighter[1]);
-////    for(i=0;i<10;i++){
-////        player1Hit= leftFighter[2]/2;
-//        player2Hit= rightFighter[2]/2;
-//        player1Damage= Math.floor(Math.random()*player1Hit);
-//        player2Damage= Math.floor(Math.random()*player2Hit);
-//        leftFighter[1]-=player1Damage;
-//        rightFighter[1]-=player2Damage;
-//        console.log(leftFighter[0]+': '+leftFighter[1]+'   '+rightFighter[0]+': '+rightFighter[1]);
-//        var result= winnerSelector();
-//        console.log(result);
-//
-//        if(result=== 'no winner'){
-//            round++;
-//            alert(leftFighter[0]+': '+leftFighter[1]+'   **Round '+round+' Over**   '+rightFighter[0]+': '+rightFighter[1]);
-//        }else{
-//            alert(result);
-//            break;
-//        }if(round== 10 && leftFighter[1]>rightFighter[1]){
-//            alert('Game Over '+leftFighter[0]+' '+'Wins!!!')
-//        }else if(round== 10 && leftFighter[1]<rightFighter[1]){
-//            alert('Game Over '+rightFighter[0]+' '+'Wins!!!')
-//        }else if(round== 10 && leftFighter[1]==rightFighter[1]){
-//            alert(result);
-//        }
-//}
-//
-//function winnerSelector(){
-//    var result= 'no winner';
-//    if(leftFighter[1]<1 && rightFighter[1]<1){
-//        result= ' DOUBLE KO!!'
-//   }else if(round== 10 && leftFighter[1]==rightFighter[1]){
-//        result= "It's a DRAW!!!"
-//   }else if(leftFighter[1]<1){
-//        result= 'Game Over '+rightFighter[0]+' '+'Wins!!!';
-//   }else if(rightFighter[1]<1){
-//        result= 'Game Over '+leftFighter[0]+' '+'Wins!!!'
-//            }
-//   return result;
-//    }
-//}
-//kingFighter();
+
